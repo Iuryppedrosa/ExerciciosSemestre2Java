92f4abc3904a9ff4f53d23df51346589fa207593
iurypedrosa <iuryppedrosa@protonmail.com>
2022-09-20 17:30:31 -0300

    152, 153, 154

---
+++ Alg152.java
@@ -0,0 +1,33 @@
+package ProfessorJean;
+
+import java.util.Scanner;
+
+public class Alg152
+{
+    public static void main(String[] args)
+    {
+        int idade;
+        double peso, gotas;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Peso: ");
+        peso = input.nextDouble();
+
+        System.out.println("Idade: ");
+        idade = input.nextInt();
+
+        gotas = 500 / 20;
+
+        if(peso < 5)
+            System.out.println("Nao pode tomar");
+        else if(idade >= 12)
+        {
+            if(peso >=60)
+            {
+                System.out.println("Tomar: " + 1000 / gotas);
+            }else System.out.println("Tomar: " + 875 / gotas);
+        }else System.out.println("Outras opcoes");
+
+
+    }
+}
